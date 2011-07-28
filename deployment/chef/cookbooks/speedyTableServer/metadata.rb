a920e14f0514b9c2f87bb8c94909c0bf7c024b7a
Eric Wright <eric@rdctd.com>
2011-07-28 19:07:27 -0400

    Add bootstrap script to install base server and cookbooks.

---
+++ deployment/chef/cookbooks/speedyTableServer/metadata.rb
@@ -0,0 +1,10 @@
+maintainer        "SpeedyTable, Inc."
+maintainer_email  "contact@speedytable.com"
+license           "Proprietary"
+description       "Installs all programs needed for a SpeedyTable server."
+version           "0.0.1"
+recipe            "speedyTableServer", "Installs all programs needed for a SpeedyTable server."
+
+%w{ fedora redhat centos ubuntu debian }.each do |os|
+  supports os
+end
---
+++ deployment/chef/solo.rb
@@ -0,0 +1,2 @@
+file_cache_path [ "/home/www-server/deployment/chef" ]
+cookbook_path [ "/home/www-server/deployment/chef/cookbooks" ]
