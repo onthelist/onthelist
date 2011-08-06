2db82643a59e5e8938069dc764013f42fbbd982d
Eric Wright <eric@rdctd.com>
2011-08-06 17:59:23 -0400

    Correct chef start script configuration path.

--- tools/startChefSolo.sh
+++ tools/startChefSolo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chef-solo -j /home/www-server/onthelist/deployment/chef/node.json
+chef-solo -j /home/www-server/onthelist/deployment/chef/node.json -c /home/www-server/onthelist/deployment/chef/solo.rb
