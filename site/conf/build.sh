c70b4fea87e0bbfdb3afd239a6f5a00256332c06
Zack Bloom <zackbloom@gmail.com>
2011-09-14 20:33:05 -0400

    Remove demo folder.

--- site/conf/build.sh
+++ site/conf/build.sh
@@ -2,4 +2,4 @@ bash -c 'mkdir -p ../public/scripts; coffee -c -o ../public/scripts ../src/scrip
 bash -c 'cd ..; mkdir -p public/styles; compass compile'
 bash -c 'cd ../src; mkdir -p ../public/html; ../../tools/jade.js -p --out ../public html'
 
-coffee push_file.coffee ../public/html/index.html `git rev-parse HEAD`.html
+coffee push_file.coffee ../public/html/index.html `GIT_DIR=~/onthelist/.git git rev-parse HEAD`.html
