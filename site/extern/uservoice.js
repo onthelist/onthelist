103c62ab08210ab5653c671a2ab3275119773525
Zack Bloom <zackbloom@gmail.com>
2011-09-16 23:07:11 -0400

    Add uservoice feedback button

---
+++ site/extern/uservoice.js
@@ -0,0 +1,8 @@
+var uvOptions = {};
+if (window.innerWidth > 900){
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/0z72gpeB87jAVSuOONavQ.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+}
