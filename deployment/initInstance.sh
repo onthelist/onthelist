6efd1ed76cc6cb7f674a873f206caa33b0865f81
Zack Bloom <zackbloom@gmail.com>
2011-09-29 12:00:26 -0700

    Switch init instance script to use master branch.

--- deployment/initInstance.sh
+++ deployment/initInstance.sh
@@ -2,7 +2,7 @@
 export NODE_PATH=/usr/local/lib/node_modules    
 
 cd /home/www-server/
-/usr/lib/git-core/git-clone -b develop git@github.com:onthelist/onthelist.git 2>&1 >> /var/log/speedy-deployment-git.log
+/usr/lib/git-core/git-clone -b master git@github.com:onthelist/onthelist.git 2>&1 >> /var/log/speedy-deployment-git.log
 
 /usr/bin/chef-solo -j /home/www-server/onthelist/deployment/chef/node.json -c /home/www-server/onthelist/deployment/chef/solo.rb 2>&1 >> /var/log/speedy-deployment-chef.log
 
