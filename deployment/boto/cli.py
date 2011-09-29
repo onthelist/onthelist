14f90447dde153ed06d2a011642922603f8ca71b
Zack Bloom <zackbloom@gmail.com>
2011-09-29 11:59:35 -0700

    Update cli to deployment config.

--- deployment/boto/cli.py
+++ deployment/boto/cli.py
@@ -8,10 +8,10 @@ import urllib2
 
 from connection import *
 
-INST_CNT = 1
+INST_CNT = 4
 LB_NAME = 'NoSSL-LB'
 MIN_OK = 4
-ZONES = ['b']
+ZONES = ['b', 'd']
 
 def status_tick():
   sys.stdout.write('.')
